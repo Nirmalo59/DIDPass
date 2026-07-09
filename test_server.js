1267bcf6ca21ba93254503191a3c32b49a2c3ee0
Nirmalo59 <shresthanirmal673@gmail.com>
2026-07-10 01:11:40 +0545

    Remove non-project files

--- test_server.js
+++
@@ -1,8 +0,0 @@
-const http = require('http');
-const server = http.createServer((req, res) => {
-  res.writeHead(200);
-  res.end('Hello from port 5002');
-});
-server.listen(5002, '127.0.0.1', () => {
-  console.log('Test server listening on 127.0.0.1:5002');
-});
